b63788e77f86a8ab59b0e737729a9404bc5ae860
Bill Lazar <blazar@gmail.com>
2013-11-16 09:30:28 -0800

    Add robots.txt module for blocking Google from non-prod environments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Abgair::Application.routes.draw do
+
+
+  get '/robots.txt' => RobotsTxt
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
